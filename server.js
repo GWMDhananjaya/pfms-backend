f33227763bd7e8230c7c9737cbdb2d36865c2eba
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-10-01 11:39:07 +0530

    ss

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const app = require('./app');
 
 // Use the port from the .env file, or default to 3307
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080 ;
 
 const server = http.createServer(app);
 
